82e64fc448e11c78a564f180209ac9a6d04f774b
Yanzz231 <mrpartys05@gmail.com>
2026-03-05 12:01:08 +0700

    feat: system run code

--- src/theme/MDXComponents.tsx
+++ src/theme/MDXComponents.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import MDXComponents from "@theme-original/MDXComponents";
 import DiffBlock from "@site/src/components/DiffBlock";
 import FileExplorer from "@site/src/components/FileExplorer";
+import RunCode from "@site/src/components/RunCode";
 
 export default {
   ...MDXComponents,
   DiffBlock,
   FileExplorer,
+  RunCode,
 };
